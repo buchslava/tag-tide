9c858b388b15c1afba0bbaef315fc038929ea409
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2021-11-18 17:08:57 +0200

    tables processing

--- test/tables.spec.ts
+++ test/tables.spec.ts
@@ -10,17 +10,14 @@ describe("tables", () => {
     const expected = "<br/><p>11 22 33 44 </p>";
     const prettified = new Prosaic(original).textTable().flatten().rootParagraphs().result();
 
-    console.log(prettified);
-
     expect(prettified).to.equal(expected);
   });
 
   it("div covered", () => {
-    const original = "<div><br /><table><tr><td>11</td><td>22<br/>999</td></tr><td>33</td><td>44</td></tr></table></div>";
+    const original =
+      "<div><br /><table><tr><td>11</td><td>22<br/>999</td></tr><td>33</td><td>44</td></tr></table></div>";
     const expected = "<p><br/>11 22 <br/>999 33 44 </p>";
     const prettified = new Prosaic(original).textTable().flatten().rootParagraphs().result();
-
-    console.log(prettified);
 
     expect(prettified).to.equal(expected);
   });
